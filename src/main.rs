458dfc9f61277d06efb5001931b19b232675c84b
Bangu7 <118105318+Bangu7@users.noreply.github.com>
2024-08-16 22:11:27 +1000

    Added music

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use macroquad;
 mod gui;
 use macroquad::prelude::*;
-
 struct Building {
     btype: String,
     width: u32,
     height: u32,
 }
 
+mod music;
+
 #[macroquad::main("Camera")]
 pub async fn main() {
+    let _music_handle = std::thread::spawn(|| {
+        music::music();
+    });
+
     loop {
         gui::gui();
 
         next_frame().await
     }
+    
 }
