62de3b860679a97d7a7203f27185bfb0eb34fb18
ZaneBehen <behenzane@gmail.com>
2024-08-17 09:33:01 +1000

    Merge conflict fixed

--- src/gui.rs
+++ src/gui.rs
@@ -1,15 +1,11 @@
 use macroquad::prelude::*;
 
 // Score implementations
-pub fn score {
-
-}
-
+pub fn score() {}
 
 pub fn gui() {
     let screen_height = screen_height();
     let screen_width = screen_width();
-
 
     // Default game frame
     // viewport, x = 0, y = 0, width, height.
@@ -25,7 +21,12 @@ pub fn gui() {
     let buy_frame = Camera2D {
         target: vec2(0.0, 0.0),
         zoom: vec2(1.0, 1.0),
-        viewport: Some(((screen_width * 0.7) as i32, 0, (screen_width * 0.3) as i32, screen_height as i32 * 2)),
+        viewport: Some((
+            (screen_width * 0.7) as i32,
+            0,
+            (screen_width * 0.3) as i32,
+            screen_height as i32 * 2,
+        )),
         ..Default::default()
     };
 
@@ -37,8 +38,6 @@ pub fn gui() {
     set_camera(&buy_frame);
     draw_rectangle(-1.0, 0.0, screen_width * 0.3, screen_height, LIGHTGRAY);
 
-
     // Reset to default camera
     set_default_camera();
 }
-
