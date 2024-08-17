79e46f217cdbae8a11d3780ce0eba370b6f6ef50
smp46 <27676057+smp46@users.noreply.github.com>
2024-08-17 10:50:24 +1000

    Added Statistics struct and initialiser, modified some existing structs

--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,51 @@ struct Building {
     btype: String, // The type of building 
     width: i32,
     height: i32,
-    sps: i32, // Students per Second that this building generates
+    students: i32, // Students per Second that this building generates
     perk_points: i32, // Number of perk points awarded by purchasing this building
 }
 
 struct Score {
-    students: i32, // Total number of students cummulated
     currStudents: i32, // Current number of avaliable students
-    dps: i32, // Dollars per second value, what is being earnt
-    dollars: i32, // Currency
+    dps: f32, // Dollars per second value, what is being earnt
+    dollars: i32, // Currency in the bank that can be spent
+    student_rate: f32 // The ratio of students/dollar (e.g. 1 student = $2/dps)
 }
 
 struct Event {
     students_awarded: i32, // Number of students this event gives (can be negative)
     event_type: String, // Type/Name of event
-    duration: i32, // How long the even lasts (seconds)
+    duration: Duration, // How long the even lasts (seconds)
     dps_modifier: i32 // Multiplier that effects overall sps rate 
 }
 
 struct GameState {
-    score: Score,
-    buildings: Vec<Building>,
-    events: Vec<Event>,
-    start_time: Instant,
+    score: Score, 
+    buildings: Vec<Building>, // Active/purchased buildings
+    events: Vec<Event>, // Active events
+    start_time: Instant, // When the game was launched
+    stats: Statistics, // Gameplay stats
+}
+
+struct Statistics {
+    total_students: u64, // Number of students ever earnt
+    total_clicks: u64, // Number of clicks ever registered
+    total_events: u64, // Number of events that have occurred
 }
 
 //Implementation's here:
 impl Score {
     fn init() -> Self {
         Score {
-            students: 0,
             currStudents: 0,
-            dps: 1,
+            dps: 1.0,
             dollars: 0,
+            student_rate: 1.0
         }
     }
 
-    fn calc_dps(&self, modifiers: Vec<i32>) -> i32 {
-        let mut new_sps: i32 = self.dps;
+    fn calc_dps(&self, modifiers: Vec<f32>) -> f32 {
+        let mut new_sps: f32 = self.dps;
         for i in modifiers.iter() {
             new_sps *= i;
         }
@@ -56,13 +63,25 @@ impl Score {
 }
 
 impl Building{
-    fn build_building(btype: String, width: i32, height: i32, sps: i32, perk_points: i32)->Building{
-        Building { btype, width, height, sps, perk_points}
+    fn build_building(btype: String, width: i32, height: i32, students: i32, perk_points: i32)->Building{
+        Building { btype, width, height, students, perk_points}
     }
 }
+
 impl Event{
-    fn build_event(students_awarded: i32, event_type: String, duration: i32, dps_modifier: i32)->Event{
+    fn build_event(students_awarded: i32, event_type: String, duration: u64, dps_modifier: i32)->Event{
+        let duration = Duration::new(duration, 0);
         Event {students_awarded, event_type, duration, dps_modifier}
+    }
+}
+
+impl Statistics {
+    fn init() -> Self {
+        Statistics {
+            total_students: 0,
+            total_clicks: 0,
+            total_events: 0,
+        }  
     }
 }
 
@@ -97,7 +116,17 @@ pub async fn main() {
     }
 }
 
-fn start_game() -> Score {
+fn start_game() -> GameState {
     let mut score: Score = Score::init();
-    score
+    let mut buildings: Vec<Building> = Vec::new();
+    let mut events: Vec<Event> = Vec::new();
+    let start_time = Instant::now();
+    let mut stats: Statistics = Statistics::init();
+    GameState {
+        score,
+        buildings,
+        events,
+        start_time,
+        stats,
+    }
 }
