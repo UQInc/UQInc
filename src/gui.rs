e26ddb8f3f1ae4738b3146b55d480c5ad09eed37
“TomMcph” <thomasmcphersonphotography@outlook.com>
2024-08-17 11:14:49 +1000

    Statistic Box

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,11 @@
 use macroquad::prelude::*;
 
+struct Student {
+    current_students: i64,
+    currency: i64,
+    dollars_ps: i64,
+}
+
 // Score implementations
 pub fn score () {
 
@@ -24,11 +30,23 @@ pub fn gui() {
         target: vec2(0.0, 0.0),
         zoom: vec2(1.0, 1.0),
         viewport: Some((
-            (screen_width * 0.7) as i32,
-            0,
-            (screen_width * 0.3) as i32,
-            screen_height as i32 * 2,
+            (screen_width * 0.7) as i32, 0, (screen_width * 0.3) as i32, screen_height as i32 * 2)),
+        ..Default::default()
+    };
+
+    let buy_frame_width = (screen_width * 0.7) / 2 as f32;
+    println!("{}", buy_frame_width);
+
+    let stat_frame = Camera2D {
+        target: vec2(0.0, 0.0),
+        zoom: vec2(1.0, 1.0),
+        viewport: Some((
+            (buy_frame_width - 800.0) as i32, 
+            (screen_height * 0.85) as i32, 
+            (screen_width * 0.7) as i32, 
+            (screen_height * 0.3) as i32
         )),
+        
         ..Default::default()
     };
 
@@ -39,6 +57,9 @@ pub fn gui() {
     // Draw the buy frame
     set_camera(&buy_frame);
     draw_rectangle(-1.0, 0.0, screen_width * 0.3, screen_height, LIGHTGRAY);
+
+    set_camera(&stat_frame);
+    draw_rectangle(0.0, 0.0, screen_width * 0.3, screen_height * 0.2, WHITE);
 
     // Reset to default camera
     set_default_camera();
