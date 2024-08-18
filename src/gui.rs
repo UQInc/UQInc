b6a079c2965f036e0c4407b8e1a0b722bae6d046
Nullzee <plasma242@gmail.com>
2024-08-18 11:38:46 +1000

    Implemented Event GUI

--- src/gui.rs
+++ src/gui.rs
@@ -310,13 +310,63 @@ pub fn draw_event_gui(event: &Event) -> bool {
     let widget_width = 500.;
     let widget_height = 400.;
 
-    let title = "New Event!";
-    let mut heading = "";
-    let mut description = "";
+    let mut heading = "".to_string();
+    let mut description = "".to_string();
+    let mut duration = "".to_string();
+    let mut effect = "".to_string();
+
+    if (*event).event_type == "CashProduction" {
+        if (*event).dps_modifier == 2. {
+            heading = "\"Inflation\"".to_string();
+            description = "Due to an increase in student fees, UQ's cash production per student is doubled".to_string();
+            effect = "Student cash producion: 2x".to_string();
+            duration.push_str("Duration: ");
+            duration.push_str(&(*event).duration.as_secs().to_string());
+            duration.push_str("s");
+        } else if (*event).dps_modifier == 0.5 {
+            heading = "International politics".to_string();
+            description = "Changes in international immigration policy have reduced UQ's income from international students".to_string();
+            effect = "Student cash producion: 0.5x".to_string();
+            duration.push_str("Duration: ");
+            duration.push_str(&(*event).duration.as_secs().to_string());
+            duration.push_str("s");
+        }
+    } else if (*event).event_type == "AddStudents" {
+        if (*event).students_awarded >= 0 {
+            heading = "Positive Media Attention".to_string();
+            description = "The ABC written a glowing piece on UQ. You gain 5% to your total students".to_string();
+            effect = "Students gained: ".to_string();
+            effect.push_str(&(*event).students_awarded.to_string());
+        } else if (*event).students_awarded >= 0 {
+            heading = "Negative Media Attention".to_string();
+            description = "UQ has been involved in a major scandal! You lose 5% of your total students".to_string();
+            effect = "Students gained: ".to_string();
+            effect.push_str(&(*event).students_awarded.to_string());
+        }
+    } else if (*event).event_type == "StudentsPerClick" {
+        if (*event).spc_modifier == 2. {
+            heading = "University Ranking Increased".to_string();
+            description = "Global university rankings have been released. UQ's rank inceased from last year. Student gain per click is doubled".to_string();
+            effect = "Student gain per click: 2x".to_string();
+            duration.push_str("Duration: ");
+            duration.push_str(&(*event).duration.as_secs().to_string());
+            duration.push_str("s");
+        } else if (*event).spc_modifier == 0.5 {
+            heading = "University Ranking Decreased".to_string();
+            description = "Global university rankings have been released. UQ's rank decreased from last year. Student gain per click is halved".to_string();
+            effect = "Student gain per click: 0.5x".to_string();
+            duration.push_str("Duration: ");
+            duration.push_str(&(*event).duration.as_secs().to_string());
+            duration.push_str("s");
+        }
+    }
 
     root_ui().window(2, vec2((screen_width / 2.) - 250., (screen_height / 2.) - 200.), vec2(500., 400.), |ui| {
-        ui.label(Vec2::new(10., 10.), "EVENT!!!");
-        if ui.button(Vec2::new(100., 100.), "Close") {
+        ui.label(Vec2::new(10., 10.), &heading);
+        ui.label(Vec2::new(10., 30.), &description);
+        ui.label(Vec2::new(10., 50.), &effect);
+        ui.label(Vec2::new(10., 70.), &duration);
+        if ui.button(Vec2::new(200., 300.), "Close") {
             outcome = false;
         }
     });
